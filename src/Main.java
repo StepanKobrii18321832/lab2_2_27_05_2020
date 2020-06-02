dcba021e9cd75c2f8c38a5b6139c152858d7af74
StepanKobrii18321832 <31667583+StepanKobrii18321832@users.noreply.github.com>
2020-06-02 13:19:41 +0300

    add dft

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,17 @@ import org.knowm.xchart.SwingWrapper;
 import org.knowm.xchart.XYChart;
 import org.knowm.xchart.XYChartBuilder;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.LongStream;
+import java.util.stream.Stream;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Lab 2.2");
 
-        Garmonic2_2 garmonic = new Garmonic2_2(14,2000, 256);
+        Garmonic2_2 garmonic = new Garmonic2_2(12,1100, 64);
         double[] i = new double[garmonic.getCountOfDescreteCalls()];
         for (int i1 = 0; i1 < i.length; i1++) {
             i[i1] = i1;
@@ -23,6 +29,21 @@ public class Main {
 
         double[] sygnals = garmonic.calculateSygnalsForResultingGarmonic();
         chart.addSeries("Fourier Function", i, garmonic.calculateFFT(sygnals));
+
+        List<Long> DFTTime = new ArrayList<>();
+        List<Long> FFTTime = new ArrayList<>();
+
+        for (int i1 = 0; i1 < 100; i1++) {
+            long time = System.currentTimeMillis();
+            garmonic.calculateFFT(sygnals);
+            FFTTime.add(System.currentTimeMillis() - time);
+
+            time = System.currentTimeMillis();
+            garmonic.calculateDFT(sygnals);
+            DFTTime.add(System.currentTimeMillis() - time);
+        }
+        System.out.println("Середній час виконання DFT: " + DFTTime.stream().mapToLong(x -> x).average().getAsDouble());
+        System.out.println("Середній час виконання FFT: " + FFTTime.stream().mapToLong(x->x).average().getAsDouble());
 
         new SwingWrapper<>(chart).displayChart();
     }
